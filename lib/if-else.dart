eccd7ded6f8ae4985d36c444f45f376b381584f9
canpurcek <canpurcek@gmail.com>
2024-08-21 07:29:34 +0000

    if-else-case

---
+++ lib/if-else.dart
@@ -0,0 +1,20 @@
+import 'dart:ffi';
+import 'dart:io';
+
+void ifElse(params) {
+  bool inputActive = true;
+
+  int inputMoney = 20;
+
+  if (inputActive) {
+    print("Kullanıcı aktif!");
+  } else {
+    print("Kullanıcı aktif değil!");
+  }
+
+  if (inputMoney > 15) {
+    print("Pahalı");
+  } else {
+    print("Ucuz!");
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,10 @@
-// Copyright 2019 the Dart project authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license
-// that can be found in the LICENSE file.
+import 'package:myapp/counter.dart';
+import 'package:myapp/if-else.dart';
 
-import 'package:flutter/material.dart';
+void main(params) {
 
-void main() => runApp(const MyApp());
+  //counter();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  ifElse(params);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorSchemeSeed: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  const MyHomePage({
-    super.key,
-    required this.title,
-  });
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
 }
