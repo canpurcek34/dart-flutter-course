ac85b2229086018e7fef9e0c5eeb0dba3f314edc
canpurcek <canpurcek@gmail.com>
2024-08-23 14:40:04 +0000

    lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_course/counter.dart';
 import 'package:flutter_course/if-else.dart';
+import 'package:flutter_course/lists.dart';
 import 'package:flutter_course/loops.dart';
 import 'package:flutter_course/methods.dart';
 import 'package:flutter_course/operators.dart';
@@ -16,5 +17,7 @@ void main(params) {
 
   //loops(params);
 
-  methods(params);
+  //methods(params);
+
+  lists(params);
 }
